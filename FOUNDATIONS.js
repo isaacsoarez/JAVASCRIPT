fa6c91657dd9fd8a0ca520a9498924c8c6fafe24
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-04 11:11:46 -0300

    Create FOUNDATIONS.js

---
+++ FOUNDATIONS.js
@@ -0,0 +1,21 @@
+_____JAVASCRIPT.JS_____
+
+console.log= print
+typeof= leia var(int, str...)
+typeof NaN= Not a Number
+typeof Infinity= Números infinitos
+console.log ('Meu nome é *****')= String
+console.log(`A soma de 2 e 4 é ${2+4}`)
+``= demonstrar função
+!= diferente
+&= e
+||= ou
+
+
+
+_____NODE.JS_____
+
+console.log(typeof true)
+console.log(typeof false)
+console.log(5>7)
+console.log(5>4 'sim' : 'nao')
